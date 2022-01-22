49b8a9e1bb7a2e7025bfd074d1a72cd1c78d2772
Lysenko Vladyslav <33177301+Vladuhalis@users.noreply.github.com>
2022-01-22 19:21:24 +0200

    Update urls.py

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import BlogListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', BlogListView.as_view(), name='home'),
 ]
+
+ 
