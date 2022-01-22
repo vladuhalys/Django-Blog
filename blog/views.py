6a7457e3f81e4e385853a3a083de1b3abd572399
Lysenko Vladyslav <33177301+Vladuhalis@users.noreply.github.com>
2022-01-22 19:58:21 +0200

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView # новое 
  
 from .models import Post
  
@@ -6,3 +6,8 @@ from .models import Post
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
+ 
+ 
+class BlogDetailView(DetailView): # новое
+    model = Post
+    template_name = 'post_detail.html'
