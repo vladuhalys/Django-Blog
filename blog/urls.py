e950161528bc7b4b11cbf949babbdab2970f1cd3
Lysenko Vladyslav <33177301+Vladuhalis@users.noreply.github.com>
2022-01-22 20:30:10 +0200

    Update post_detail

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from blog.views import BlogDetailView, BlogListView, BlogCreateView
 
 urlpatterns = [
-    path('post/new/', BlogCreateView.as_view(), name='post_new'), # new
+    path('post/new/', BlogCreateView.as_view(), name='post_new'), 
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
     path('', BlogListView.as_view(), name='home'),
 ]
