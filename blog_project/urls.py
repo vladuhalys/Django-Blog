cc860bf5eddd58412a8e67726ab856b5c6d0653c
Lysenko Vladyslav <33177301+Vladuhalis@users.noreply.github.com>
2022-01-22 19:43:39 +0200

    Urls update

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -14,13 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from . import BlogListView
-
+from django.urls import path, include # новое изменение
+ 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', BlogListView.as_view(), name='home'),
-    path('', include('blog.urls')),
+    path('', include('blog.urls')), # новое изменение
 ]
-
  
