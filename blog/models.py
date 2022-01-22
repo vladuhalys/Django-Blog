a98b90fe27770569576ab3ee878b7233b696e877
Lysenko Vladyslav <33177301+Vladuhalis@users.noreply.github.com>
2022-01-22 20:27:51 +0200

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
-# blog/models.py
 from django.db import models
+from django.urls import reverse # Новый импорт
  
  
 class Post(models.Model):
@@ -12,5 +12,6 @@ class Post(models.Model):
  
     def __str__(self):
         return self.title
-
-# Create your models here.
+ 
+    def get_absolute_url(self): 
+        return reverse('post_detail', args=[str(self.id)])
